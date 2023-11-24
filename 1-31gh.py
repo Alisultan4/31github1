ca3889b02f561f6cb458d68aa9375782f8ee3bba
alisa00zhanov <akimganofalisultan@gmail.com>
2023-11-25 02:28:56 +0600

    1-31gh.py

---
+++ 1-31gh.py
@@ -0,0 +1,23 @@
+hw =  print('Hello world')
+c = 'nurik'
+b = 'sandzhik'
+a = 'beko'
+d = 'ars'
+e = 'atanai'
+f = 'akhror'
+g = 'chika'
+i = 'amir'
+j = 'aitema'
+k = 'alisa'
+l = 'akni'
+m = 'alizhan'
+n = 'aiman'
+o = 'beka'
+p = 'andrew'
+t = 'zhanay'
+u = 'adilet'
+v = 'yunus'
+w = 'atai'
+x = 'nurhan'
+elevenc = a,b,c,d,e,f,g,i,j,k,l,m,n,o,p,t,u,v,w,x
+print(f'{hw} {elevenc}')
