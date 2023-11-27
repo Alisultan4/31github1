724fbee0150291ad558ea712819134d7eaed3876
Alisa4 <145136723+Alisultan4@users.noreply.github.com>
2023-11-27 17:49:26 +0600

    Update 2-31gh.py

--- 2-31gh.py
+++ 2-31gh.py
@@ -14,7 +14,7 @@
 # Выходные данные
 # Выведите n строк. В i строке должен находиться результат замены i-го слова из входных данных.
 
-list1 = "localization","internationalization","pseudoscience"
+list1 = "localization","internationalization"
 
 def string_splitter(x):
     c = list(x)
